2f7b12324f6184faf57ca0ea7ecd692213dab6a3
Abdulellah Hakim <abdulelahhakeem@abdulelsmacbook.ict.ad.dit.ie>
2015-09-30 15:05:47 +0100

    updated code

--- Lab3/Palindrome.py
+++ Lab3/Palindrome.py
@@ -1,8 +1,13 @@
+my_str = input("Enter a string: ")
 
-def isPalindrome(num):
-     if num[::-1] == num:
-       return True
-    else:
-       return False
+# make it suitable for caseless comparison
+my_str = my_str.casefold()
 
-print('Hello Alex')
+# reverse the string
+rev_str = reversed(my_str)
+
+# check if the string is equal to its reverse
+if list(my_str) == list(rev_str):
+   print("It is palindrome")
+else:
+   print("It is not palindrome")
